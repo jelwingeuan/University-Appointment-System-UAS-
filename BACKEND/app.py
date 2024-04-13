da18c1a0aa6e33a0dda2fe46f09aa7cf56694310
kimhongzhang <kim.hong.zhang323@gmail.com>
2024-04-13 21:15:28 +0800

    v1.0.3 (Frontend Uploaded without integration)

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import auth
 
 app = Flask(__name__)
@@ -6,6 +6,9 @@ app = Flask(__name__)
 # Registers the 'auth' blueprint with the application. #
 app.register_blueprint(auth)
 
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
