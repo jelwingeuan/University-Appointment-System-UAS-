84da22e426d0c3cbb6898988d3873b10d14d9e14
kimhongzhang <kim.hong.zhang323@gmail.com>
2024-04-18 12:34:12 +0800

    v1.0.9

--- website/views.py
+++ website/views.py
@@ -2,6 +2,3 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
-@auth.route("/")
-def home():
-    return render_template("home.html")
